2245109112457e1505aa77a82e51a7940a343ac9
eunggyo <eungkyo89@gamil.com>
2022-02-08 20:51:59 +0900

    권한 추가 구현 - 구현중이며 중간커밋

---
+++ app/src/main/java/com/keg/bluetoothscanner/Constant.java
@@ -0,0 +1,5 @@
+package com.keg.bluetoothscanner;
+
+public class Constant {
+    public static final String PERMISSION_ACCESS_FINE_LOCATION = "Manifest.permission.ACCESS_FINE_LOCATION";
+}
--- app/src/main/java/com/keg/bluetoothscanner/ScanFragment.java
+++ app/src/main/java/com/keg/bluetoothscanner/ScanFragment.java
@@ -1,7 +1,9 @@
 package com.keg.bluetoothscanner;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -26,5 +28,27 @@ public class ScanFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_scan, container, false);
+    }
+
+
+    /*
+        ACCESS_FINE_LOCATION 권한을 가지고 있는지 여부를 리턴하는 메소드.
+        return true ACCESS_FINE_LOCATION 권한을 가지고있다.
+        return false
+     */
+    private boolean hasPermission() {
+        if(ContextCompat.checkSelfPermission(getActivity(),Constant.PERMISSION_ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
+                return true;
+        }else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+        if(!hasPermission()) {
+
+        }else {
+
+        }
     }
 }
